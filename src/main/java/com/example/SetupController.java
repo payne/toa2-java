bd98d95a977d9da8357e55d46ee4ba65ef5230c7
Matt Payne <Payne@MattPayne.org>
2019-02-24 15:25:48 -0600

    Reading from classpath experiment.

--- src/main/java/com/example/SetupController.java
+++ src/main/java/com/example/SetupController.java
@@ -1,4 +1,5 @@
 package com.example;
+
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.sql.DataSource;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,4 +26,18 @@ import java.util.Map;
 public class SetupController {
 	@Autowired
 	private DataSource dataSource;
+
+	@RequestMapping("/db")
+	String db(Map<String, Object> model) {
+		try {
+			String value = new String(Files.readAllBytes(Paths.get(getClass().getResource("schema.sql").toURI())));
+
+			model.put("msg", value);
+
+		} catch (Exception e) {
+			model.put("msg", e.toString());
+		}
+
+		return "setup";
+	}
 }
