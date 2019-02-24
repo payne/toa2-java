560a23a40ab7901896d53d1fdb2770d4e1bd579b
Matt Payne <Payne@MattPayne.org>
2019-02-24 16:12:04 -0600

    try commons io 1

--- src/main/java/com/example/SetupController.java
+++ src/main/java/com/example/SetupController.java
@@ -2,6 +2,8 @@ package com.example;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.sql.DataSource;
 
+import java.io.File;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
@@ -30,7 +34,12 @@ public class SetupController {
 	@RequestMapping("/setup")
 	String db(Map<String, Object> model) {
 		try {
-			String value = new String(Files.readAllBytes(Paths.get(getClass().getResource("schema.sql").toURI())));
+			//String value = new String(Files.readAllBytes(Paths.get(getClass().getResource("schema.sql").toURI())));
+			
+			URL url = getClass().getResource("schema.sql");
+			File myFile = new File(url.toURI());
+
+			String value = FileUtils.readFileToString(myFile, "UTF-8"); 
 
 			model.put("msg", value);
 
