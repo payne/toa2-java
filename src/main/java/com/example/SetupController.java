3f1697e3bfd60919997a67754874539748b61466
Matt Payne <Payne@MattPayne.org>
2019-02-24 14:39:08 -0600

    Try adding a schema.sql - will it work?

---
+++ src/main/java/com/example/SetupController.java
@@ -0,0 +1,24 @@
+package com.example;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+
+@Controller
+public class SetupController {
+	@Autowired
+	private DataSource dataSource;
+}
