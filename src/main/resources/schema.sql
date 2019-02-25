06d1fe673d3667787cba75f9cc762447a2b5e1f5
Matt Payne <Payne@MattPayne.org>
2019-02-24 18:23:36 -0600

    people and tag tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,45 +1,21 @@
-CREATE TABLE IF NOT EXISTS vets (
-  id SERIAL PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30)
-);
-
-CREATE TABLE IF NOT EXISTS specialties (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(80)
-);
-
-CREATE TABLE IF NOT EXISTS vet_specialties (
-  vet_id INT NOT NULL,
-  specialty_id INT NOT NULL,
-  UNIQUE (vet_id,specialty_id)
-);
-
-CREATE TABLE IF NOT EXISTS types (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(80)
-);
-
-CREATE TABLE IF NOT EXISTS owners (
+CREATE TABLE IF NOT EXISTS people (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  address VARCHAR(255),
-  city VARCHAR(80),
-  telephone VARCHAR(20)
+  email VARCHAR(30),
+  description VARCHAR(2048)
 );
 
-CREATE TABLE IF NOT EXISTS pets (
+CREATE TABLE IF NOT EXISTS tags (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(30),
-  birth_date DATE,
-  type_id INT NOT NULL,
-  owner_id INT NOT NULL
+  tag VARCHAR(80)
 );
 
-CREATE TABLE IF NOT EXISTS visits (
-  id SERIAL PRIMARY KEY,
-  pet_id INT NOT NULL,
-  visit_date DATE,
-  description VARCHAR(255)
+CREATE TABLE IF NOT EXISTS tagToPerson (
+  id INT NOT NULL,
+  person_id INT,
+  tag_id INT,
+  ambassador BOOLEAN default false
 );
+
+
